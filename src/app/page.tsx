b277ae25895a053c701776aef2057b6fc4f243c4
mitchellabrouwer <mbarron198@gmail.com>
2023-11-17 09:17:41 +1100

    fix: head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import Head from "next/head";
 import Dashboard from "../components/Dashboard";
 
 export default function Home() {
   return (
     <main className="px-1 pb-10 pt-[45px] md:px-12">
+      <Head>
+        <title />
+        <meta
+          name="Centsiblility"
+          content="Centsibility is a privacy-focused financial app that categorizes transactions from uploaded CSV files on-device, ensuring user data anonymity and discarding data post-processing without retention or backups."
+        />
+      </Head>
       <Dashboard />
     </main>
   );
